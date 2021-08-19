3d9a8fb99a166914b573ab837260f9e9df8dbd6b
parentda <danielparent21@gmail.com>
2021-08-19 13:21:30 -0400

    updated Player class to avoid multiple players having the same marker

--- player.rb
+++ player.rb
@@ -2,6 +2,7 @@
 
 class Player
   @@player_number = 1
+  @@taken_markers = []
   attr_reader :name, :marker
 
   def initialize
@@ -10,10 +11,11 @@ class Player
 
     puts "Hello #{@name}, please enter a single character to serve as a marker for your chosen spaces: "
     @marker = gets.chomp
-    until @marker.length == 1
-      puts 'Please re-enter your marker choice and ensure that it is a single character: '
+    until @marker.length == 1 && !@@taken_markers.include?(@marker)
+      puts 'Please re-enter your marker choice and ensure that it is a single character that is not already being used by another player: '
       @marker = gets.chomp
     end
+    @@taken_markers << @marker
 
     @@player_number += 1
   end
