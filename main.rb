89b280bd079221ebeb3049f46b342015ef7e5c9c
parentda <danielparent21@gmail.com>
2021-08-19 11:18:02 -0400

    completed Player class

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'board'
+require_relative 'game'
+require_relative 'player'
+
 puts 'Player 1, please enter your name: '
 test = gets
 puts "Hello #{test}"
--- player.rb
+++ player.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Player
+  attr_reader :name, :symbol
+
+  def initialize(player_number)
+    puts "Player #{player_number}, please enter your name: "
+    @name = gets.chomp
+    puts "Hello #{@name}, please enter a single character to serve as a marker for your chosen spaces: "
+    @symbol = gets.chomp
+    until @symbol.length == 1
+      puts 'Please re-enter your marker choice and ensure that it is a single character: '
+      @symbol = gets.chomp
+    end
+  end
 end
